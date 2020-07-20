870699ef50506b25e8b578a518ddccc5c5822c81
Chams <chams.aidel@outlook.fr>
2020-07-21 01:32:07 +0200

    ex4

--- ex04/ft_strs_to_tab.c
+++ ex04/ft_strs_to_tab.c
@@ -6,8 +6,63 @@
 /*   By: caidel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 18:14:03 by caidel            #+#    #+#             */
-/*   Updated: 2020/07/20 18:14:20 by caidel           ###   ########.fr       */
+/*   Updated: 2020/07/21 01:31:22 by caidel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_strs_to_tab.h"
 
+char				*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = src[i];
+	return (dest);
+}
+
+int					ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char				*ft_strdup(char *src)
+{
+	char *s;
+
+	if (!(s = (char*)malloc(sizeof(*s) * ft_strlen(src))))
+		return (0);
+	ft_strcpy(s, src);
+	return (s);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	int					i;
+	struct s_stock_str	*s_tab;
+
+	i = 0;
+	if (!(s_tab = malloc(sizeof(*s_tab) * (ac + 1))))
+		return (0);
+	while (i < ac)
+	{
+		s_tab[i].size = ft_strlen(av[i]);
+		s_tab[i].str = av[i];
+		s_tab[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	s_tab[i].size = 0;
+	s_tab[i].str = 0;
+	s_tab[i].copy = 0;
+	return (s_tab);
+}
